3af320159b591504162a5da57e3f6bad9ecc9dd1
Sebastian Kraemer <basti.kr@gmail.com>
2018-12-14 06:41:00 +0100

    Add test fir is_in_con predicate.

---
+++ tests/test_is_in_cone.cpp
@@ -0,0 +1,31 @@
+#include "src/triangulation.cpp"
+#include "gtest/gtest.h"
+
+using namespace bakr;
+
+
+TEST(PREDICATES, IN_CONE) {
+  EXPECT_TRUE(is_in_cone({3, -4}, {3, -5}, {4, -5}, {4, -4}));
+  EXPECT_TRUE(is_in_cone({3, -4}, {3, -5}, {4, -5}, {10000, -4}));
+  EXPECT_TRUE(is_in_cone({3, -4}, {3, -5}, {4, -5}, {4, 10000}));
+
+  EXPECT_FALSE(is_in_cone({3, -4}, {3, -5}, {4, -5}, {2, 10000}));
+  EXPECT_FALSE(is_in_cone({3, -4}, {3, -5}, {4, -5}, {10000, -6}));
+  EXPECT_FALSE(is_in_cone({3, -4}, {3, -5}, {4, -5}, {2, -6}));
+
+  // Reverse order
+  EXPECT_FALSE(is_in_cone({4, -5}, {3, -5}, {3, -4}, {4, -4}));
+  EXPECT_FALSE(is_in_cone({4, -5}, {3, -5}, {3, -4}, {10000, -4}));
+  EXPECT_FALSE(is_in_cone({4, -5}, {3, -5}, {3, -4}, {4, 10000}));
+
+  EXPECT_TRUE(is_in_cone({4, -5}, {3, -5}, {3, -4}, {2, 10000}));
+  EXPECT_TRUE(is_in_cone({4, -5}, {3, -5}, {3, -4}, {10000, -6}));
+  EXPECT_TRUE(is_in_cone({4, -5}, {3, -5}, {3, -4}, {2, -6}));
+
+  // Edge cases
+  EXPECT_FALSE(is_in_cone({3, -4}, {3, -5}, {4, -5}, {3, -4}));
+  EXPECT_FALSE(is_in_cone({3, -4}, {3, -5}, {4, -5}, {3, -5}));
+  EXPECT_FALSE(is_in_cone({3, -4}, {3, -5}, {4, -5}, {4, -5}));
+  EXPECT_FALSE(is_in_cone({3, -4}, {3, -5}, {4, -5}, {3, 10000}));
+  EXPECT_FALSE(is_in_cone({3, -4}, {3, -5}, {4, -5}, {10000, -5}));
+}
